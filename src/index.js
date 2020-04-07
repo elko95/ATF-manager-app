eee7a86f53a85432c24cee3078970bd0b2a28a5a
Khaoula Elkoi <elkokhaoula@gmail.com>
2020-04-07 16:51:58 +0100

    add CORS to index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,29 @@ const domaineRouter = require("./routers/domaine");
 const app = express();
 const port = process.env.PORT;
 
+// Create express server
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+
+  app.options("*", (req, res) => {
+    // allowed XHR methods
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+    );
+    res.send();
+  });
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(tableRouter);
