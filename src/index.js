7b87b0b17054746a6a761ee414367d19df87d334
Khaoula Elkoi <elkokhaoula@gmail.com>
2020-04-08 00:32:12 +0100

    CORS 2

--- src/index.js
+++ src/index.js
@@ -5,30 +5,32 @@ const tableRouter = require("./routers/table");
 const processusRouter = require("./routers/processus");
 const domaineRouter = require("./routers/domaine");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT;
 
 // Create express server
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
 
-  // authorized headers for preflight requests
-  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+//   // authorized headers for preflight requests
+//   // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
 
-  app.options("*", (req, res) => {
-    // allowed XHR methods
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
-    );
-    res.send();
-  });
-});
+//   app.options("*", (req, res) => {
+//     // allowed XHR methods
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+//     );
+//     res.send();
+//   });
+// });
 
 app.use(express.json());
 app.use(userRouter);
